85ae33ff16757d0e3fc73a4bc1d65cd6917b2c40
Levi Kilcher <levi.kilcher@gmail.com>
2013-05-10 15:46:39 -0600

    Updated README file, added warning statement for TSlib, and made other documenation additions.

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# The setup script for creating a stand-alone HydroTurbSim windows (.exe) file.
 from distutils.core import setup
 import py2exe, sys
 
@@ -7,4 +8,3 @@ setup(console=['HydroTurbSim.py'],
       options={'py2exe':{'bundle_files':1,'compressed':'True'}},
       zipfile = None,
       )
-
