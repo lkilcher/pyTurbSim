ceb0f38e2a6a90f3babb4d19c8eaca1d019537ed
Levi Kilcher <levi.kilcher@gmail.com>
2015-02-18 16:01:22 -0700

    Update version for a new pypi upload.

--- pyts/_version.py
+++ pyts/_version.py
@@ -2,8 +2,8 @@
 This module contains PyTurbSim version information.
 """
 __prog_name__ = 'PyTurbSim'
-__version__ = '0.3.5'
-__version_date__ = 'Nov-26-2014'
+__version__ = '0.3.6'
+__version_date__ = 'Feb-18-2014'
 pkg_name = 'pyts'
 
 VerString = __prog_name__ + ' v' + __version__
