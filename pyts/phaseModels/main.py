6905d6e318dee908415ca3c1bdddc8e135d82255
Levi Kilcher <Levi.Kilcher@gmail.com>
2018-01-08 20:56:57 -0700

    Create skeleton for Temporal Coherence.

--- pyts/phaseModels/main.py
+++ pyts/phaseModels/main.py
@@ -44,3 +44,34 @@ class Uniform(phaseModelBase):
         Phase model used                                 =  {dat.model_desc}
         """
         return sumstring_format.format(dat=self,)
+
+
+class Rinker(phaseModelBase):
+    """The temporal-coherence phase model
+
+    This is based on the work of Jenni Rinker:
+    http://dx.doi.org/10.1007/s10546-015-0121-x
+    """
+
+    def __init__(self, rho, mu=np.pi):
+        self.rho = rho
+        self.mu = mu
+
+    def __call__(self, tsrun):
+        phases = phaseObj(tsrun)
+
+        # phases.array[:] = ...
+        # This is where we need to define phases.array (equivalently,
+        # phases[:]). Note that it is a n_comp=3, by n_p
+        # (number of z,y points) by n_f (num frequencies) array.
+
+        # Then return it...
+        return phases
+
+    def _sumfile_string(self, tsrun, ):
+        sumstring_format = """
+        Phase model used                                 =  {dat.model_desc}
+        rho                                              =  {dat.rho:0.4g}
+        mu                                               =  {dat.mu:0.4g}
+        """
+        return sumstring_format.format(dat=self,)
